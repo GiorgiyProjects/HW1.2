69ae4ac4c2098175bfec10f7d3512654ec2e5f1c
Giorgiy <GiorgiyProjects@gmail.com>
2021-05-23 15:15:06 +0300

    code review changes

---
+++ FilterLibrary.hpp
@@ -0,0 +1,95 @@
+#include <fstream>
+#include <iostream>
+
+using pool = std::vector<std::vector<int>>;
+
+// ("",  '.') -> [""]
+// ("11", '.') -> ["11"]
+// ("..", '.') -> ["", "", ""]
+// ("11.", '.') -> ["11", ""]
+// (".11", '.') -> ["", "11"]
+// ("11.22", '.') -> ["11", "22"]
+std::vector<std::string> split(const std::string &str, char d)
+{
+    std::vector<std::string> r;
+
+    std::string::size_type start = 0;
+    std::string::size_type stop = str.find_first_of(d);
+    while(stop != std::string::npos)
+    {
+        r.push_back(str.substr(start, stop - start));
+
+        start = stop + 1;
+        stop = str.find_first_of(d, start);
+    }
+
+    r.push_back(str.substr(start));
+
+    return r;
+}
+
+void readPoolFromFile(pool& ip_pool, std::ifstream& file)
+{
+    for(std::string line; std::getline(file, line);)
+    {
+        std::vector<std::string> v = split(line, '\t');
+        auto ip_filter_strings = split(v.at(0), '.');
+        std::vector<int> ip_filter_ints;
+        std::transform(ip_filter_strings.begin(), ip_filter_strings.end(), std::back_inserter(ip_filter_ints),
+                       [](const std::string& str) { return std::stoi(str);});
+        ip_pool.push_back(ip_filter_ints);
+    }
+}
+
+template <class T>
+inline std::ostream& operator << (std::ostream& os, const std::vector<T>& ip_pool)
+{
+    for(const auto& ip : ip_pool)
+    {
+        std::string c = "";
+        for(const auto& ip_part : ip)
+        {
+            os << c << ip_part;
+            c = ".";
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
+void LexicographicSort(pool& ip_pool)
+{
+    sort(ip_pool.begin(), ip_pool.end(), [] (std::vector<int> v1, std::vector<int> v2)
+    {
+        for (int i = 0; i < std::min(v1.size(), v2.size()); ++i)
+        {
+            if (v1[i] != v2[i])
+            {
+                return (v1[i] > v2[i]);
+            }
+        }
+        return  true;
+    });
+    return;
+}
+
+auto startFromOne = [](const std::vector<int>& v)
+{
+    return (v[0] == 1);
+};
+
+auto first46Second70 = [](const std::vector<int>& v)
+{
+    return ((v[0] == 46) && (v[1] == 70));
+};
+
+auto any46 = [](const std::vector<int>& v)
+{
+    return (find(v.begin(), v.end(), 46) != v.end());
+};
+
+template<typename T>
+void poolFilter(pool& in, pool& out, T functor)
+{
+    copy_if(in.begin(), in.end(), back_inserter(out), functor);
+}
--- ip_filter.cpp
+++ ip_filter.cpp
@@ -6,102 +6,37 @@
 #include <fstream>
 #include<algorithm>
 #include <cstdarg>
-
-// ("",  '.') -> [""]
-// ("11", '.') -> ["11"]
-// ("..", '.') -> ["", "", ""]
-// ("11.", '.') -> ["11", ""]
-// (".11", '.') -> ["", "11"]
-// ("11.22", '.') -> ["11", "22"]
-std::vector<std::string> split(const std::string &str, char d)
-{
-    std::vector<std::string> r;
-
-    std::string::size_type start = 0;
-    std::string::size_type stop = str.find_first_of(d);
-    while(stop != std::string::npos)
-    {
-        r.push_back(str.substr(start, stop - start));
-
-        start = stop + 1;
-        stop = str.find_first_of(d, start);
-    }
-
-    r.push_back(str.substr(start));
-
-    return r;
-}
-
-template <class T>
-inline std::ostream& operator << (std::ostream& os, const std::vector<T>& ip_pool)
-{
-    for(const auto& ip : ip_pool)
-    {
-        std::string c = "";
-        for(const auto& ip_part : ip)
-        {
-            os << c << ip_part;
-            c = ".";
-        }
-        os << std::endl;
-    }
-    return os;
-}
+#include "FilterLibrary.hpp"
 
 using namespace std;
 int main(int argc, char const *argv[])
 {
     try
     {
-        std::vector<std::vector<std::string> > ip_pool;
-
-        for(std::string line; std::getline(std::cin, line);)
-        {
-            std::vector<std::string> v = split(line, '\t');
-            ip_pool.push_back(split(v.at(0), '.'));
-        }
+        pool ip_pool;
+        ifstream file;
+        file.open("../ip_filter.tsv");
+        readPoolFromFile(ip_pool, file);
+        file.close();
 
         // TODO reverse lexicographic sort
-        sort(ip_pool.begin(), ip_pool.end(), [] (vector<string> v1, vector<string> v2)
-        {
-            for (int i = 0; i < std::min(v1.size(), v2.size()); ++i)
-            {
-                if (v1[i] != v2[i])
-                {
-                    return (std::stoi(v1[i]) > std::stoi(v2[i]));
-                }
-            }
-            return  true;
-        });
+        LexicographicSort(ip_pool);
         cout << ip_pool;
 
         // TODO filter by first byte and output
-        // ip = filter(1)
-        vector<vector<string>> filtered_pool1;
-        copy_if(ip_pool.begin(), ip_pool.end(), back_inserter(filtered_pool1), [](vector<string> v)
-        {
-            return stoi(v[0]) == 1;
-        });
+        pool filtered_pool1;
+        poolFilter(ip_pool, filtered_pool1, startFromOne);
         cout << filtered_pool1;
 
         // TODO filter by first and second bytes and output
-        // ip = filter(46, 70)
-        vector<vector<string>> filtered_pool2;
-        copy_if(ip_pool.begin(), ip_pool.end(), back_inserter(filtered_pool2), [](vector<string> v)
-        {
-            return (stoi(v[0]) == 46 && stoi(v[1]) == 70);
-        });
+        pool filtered_pool2;
+        poolFilter(ip_pool, filtered_pool2, first46Second70);
         cout << filtered_pool2;
 
         // TODO filter by any byte and output
-        // ip = filter_any(46)
-        vector<vector<string>> filtered_pool3;
-        copy_if(ip_pool.begin(), ip_pool.end(), back_inserter(filtered_pool3), [](vector<string> v)
-        {
-            return (find(v.begin(), v.end(), "46") != v.end());
-        });
+        pool filtered_pool3;
+        poolFilter(ip_pool, filtered_pool3, any46);
         cout << filtered_pool3;
-
     }
     catch(const std::exception &e)
     {
